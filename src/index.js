02cb72b6b07ff96fc02939e76220f80800ad9958
unknown <mateox463@gmail.com>
2026-03-09 04:36:27 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// npm init -y
+// git init
+// npm install prisma -D
+// npm install dotenv -D
+// npm install @prisma/client
+// npm install @prisma/adapter-pg
+// npm install pg
+
+// Ya despues de eso podemos usar npx prisma init para empezar a configurar nuestro proyecto, esto nos va a crear una carpeta llamada prisma con un archivo schema.prisma y un .env en la raiz del proyecto
